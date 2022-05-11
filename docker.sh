ad25802d8a2eec3168a20e605d055882f719c79c
rafikarhaiem <rafikarhaiem@gmail.com>
2022-05-11 19:17:44 +0100

    Sherlock modify

--- docker.sh
+++ docker.sh
@@ -2,10 +2,10 @@
 #docker build -t rafikarhaiem/sherlock:v1 .
 #docker push rafikarhaiem/sherlock:v1
 #docker run --rm -t -v "$PWD/results:/opt/sherlock/results" rafikarhaiem/sherlock:v1 -o /opt/sherlock/results/list.txt rafikarhaiem
-git init
-git config --global --add safe.directory /home/rafika/Bureau/docker/sherlock-master
+#git init
+#git config --global --add safe.directory /home/rafika/Bureau/docker/sherlock-master
 git add .
-git commit -m "Sherlock"
+git commit -m "Sherlock modify"
 git branch -M main
 git remote add origin git@github.com:rafikarhaiem/sherlock.git
 git push -u origin main
